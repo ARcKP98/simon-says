cf2effdab4df3b62c534913eb5eb5fdc65a36a58
ARcKP98 <pkartik4386@outlook.com>
2023-03-14 10:15:05 +0000

    Test game function.

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+const { game } = require("../game");
+
 
 beforeAll(() => {
     let fs = require("fs");
@@ -9,4 +11,19 @@ beforeAll(() => {
     document.open();
     document.write(fileContents);
     document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
 });
